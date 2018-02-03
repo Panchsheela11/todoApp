d3f5e8b624786d6a9e452b583bbd5dab1df9e2a4
Panchsheela <panchsheela92@gmail.com>
2018-02-03 12:50:30 +0530

    1st part of tuutorial completed

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+import React, { Component } from 'react';
+import Main from './app/components/Main';
+import {
+  View,
+  Text
+} from 'react-native'
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+
